9ee4469e982a11816763e0bce75b4946c050e521
luis jara <xextremex06@gmail.com>
2023-09-18 10:49:05 -0400

    fixed page title

--- CarPool/monitorview-classroom.php
+++ CarPool/monitorview-classroom.php
@@ -21,7 +21,7 @@ $sec = 3;
 <html>
 
 <head>
-    <title>Monitor - CarPool Management</title>
+    <title>Monitor Classroom - CarPool Management</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/dark.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
