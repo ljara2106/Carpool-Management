d5d607a516b98bd8e2979c52cc33b2e942594795
luis jara <xextremex06@gmail.com>
2023-11-27 11:05:34 -0500

    signup using cloudflare turnstile

---
+++ CarPool/process-signup-grecaptcha.php
@@ -0,0 +1,74 @@
+<?php
+
+if (empty($_POST["name"])) {
+    die("Name is required");
+}
+
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    die("Valid email is required");
+}
+
+if (strlen($_POST["password"]) < 8) {
+    die("Password must be at least 8 characters");
+}
+
+if (!preg_match("/[a-z]/i", $_POST["password"])) {
+    die("Password must contain at least one letter");
+}
+
+if (!preg_match("/[0-9]/", $_POST["password"])) {
+    die("Password must contain at least one number");
+}
+
+if ($_POST["password"] !== $_POST["password_confirmation"]) {
+    die("Passwords must match");
+}
+
+if (isset($_POST['g-recaptcha-response'])) {
+
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $secret = 'YOUR_SECRET_KEY';
+    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret .  '&remoteip' . $ip . '&response=' . $_POST['g-recaptcha-response']);
+    $responseData = json_decode($verifyResponse);
+    if ($responseData->success) {
+
+        $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+        $mysqli = require __DIR__ . "/dbconfig/database.php";
+
+        $sql = "INSERT INTO user (name, email, password_hash, teacher_id)
+                    VALUES (?, ?, ?, ?)";
+
+        $stmt = $mysqli->stmt_init();
+
+        if (!$stmt->prepare($sql)) {
+            die("SQL error: " . $mysqli->error);
+        }
+
+        // Check if teacher_id is provided and is a valid integer, otherwise set it to NULL
+        $teacher_id = isset($_POST["teacher_id"]) && is_numeric($_POST["teacher_id"]) ? $_POST["teacher_id"] : NULL;
+
+        $stmt->bind_param(
+            "ssss",
+            $_POST["name"],
+            $_POST["email"],
+            $password_hash,
+            $teacher_id
+        );
+
+        if ($stmt->execute()) {
+
+            header("Location: signup-success.html");
+            exit;
+        } else {
+
+            if ($mysqli->errno === 1062) {
+                die("email already taken");
+            } else {
+                die($mysqli->error . " " . $mysqli->errno);
+            }
+        }
+    } else {
+        die("Please check reCAPTCHA");
+    }
+}
--- CarPool/process-signup.php
+++ CarPool/process-signup.php
@@ -12,7 +12,7 @@ if (strlen($_POST["password"]) < 8) {
     die("Password must be at least 8 characters");
 }
 
-if (!preg_match("/[a-z]/i", $_POST["password"])) {
+if (!preg_match("/[a-zA-Z]/", $_POST["password"])) {
     die("Password must contain at least one letter");
 }
 
@@ -24,51 +24,63 @@ if ($_POST["password"] !== $_POST["password_confirmation"]) {
     die("Passwords must match");
 }
 
-if (isset($_POST['g-recaptcha-response'])) {
-
+if (isset($_POST['cf-turnstile-response'])) {
     $ip = $_SERVER['REMOTE_ADDR'];
-    $secret = '6LdtiwwUAAAAAAfXRgEh9zg37goVaeuRL1btdONl';
-    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret .  '&remoteip' . $ip . '&response=' . $_POST['g-recaptcha-response']);
-    $responseData = json_decode($verifyResponse);
-    if ($responseData->success) {
+    $secret = 'YOUR_SECRET_KEY';
 
-        $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $postData = [
+        'secret' => $secret,
+        'remoteip' => $ip,
+        'response' => $_POST['cf-turnstile-response'],
+    ];
 
-        $mysqli = require __DIR__ . "/dbconfig/database.php";
+    $ch = curl_init('https://challenges.cloudflare.com/turnstile/v0/siteverify');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
 
-        $sql = "INSERT INTO user (name, email, password_hash, teacher_id)
-                    VALUES (?, ?, ?, ?)";
+    $verifyResponse = curl_exec($ch);
 
-        $stmt = $mysqli->stmt_init();
+    if ($verifyResponse !== false) {
+        $responseData = json_decode($verifyResponse);
 
-        if (!$stmt->prepare($sql)) {
-            die("SQL error: " . $mysqli->error);
-        }
+        if ($responseData && isset($responseData->success) && $responseData->success === true) {
+            $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-        // Check if teacher_id is provided and is a valid integer, otherwise set it to NULL
-        $teacher_id = isset($_POST["teacher_id"]) && is_numeric($_POST["teacher_id"]) ? $_POST["teacher_id"] : NULL;
+            // Include database configuration
+            require __DIR__ . "/dbconfig/database.php";
 
-        $stmt->bind_param(
-            "ssss",
-            $_POST["name"],
-            $_POST["email"],
-            $password_hash,
-            $teacher_id
-        );
+            $sql = "INSERT INTO user (name, email, password_hash, teacher_id) VALUES (?, ?, ?, ?)";
+            $stmt = $mysqli->stmt_init();
 
-        if ($stmt->execute()) {
+            if ($stmt->prepare($sql)) {
+                // Check if teacher_id is provided and is a valid integer, otherwise set it to NULL
+                $teacher_id = isset($_POST["teacher_id"]) && is_numeric($_POST["teacher_id"]) ? $_POST["teacher_id"] : NULL;
 
-            header("Location: signup-success.html");
-            exit;
-        } else {
+                $stmt->bind_param("ssss", $_POST["name"], $_POST["email"], $password_hash, $teacher_id);
 
-            if ($mysqli->errno === 1062) {
-                die("email already taken");
+                if ($stmt->execute()) {
+                    header("Location: signup-success.html");
+                    exit;
+                } else {
+                    if ($mysqli->errno === 1062) {
+                        die("Email already taken");
+                    } else {
+                        die("Database error: " . $mysqli->error);
+                    }
+                }
             } else {
-                die($mysqli->error . " " . $mysqli->errno);
+                die("SQL error: " . $mysqli->error);
             }
+        } else {
+            die("Please check reCAPTCHA");
         }
     } else {
-        die("Please check reCAPTCHA");
+        // Handle cURL error
+        $error_message = 'cURL error: ' . curl_error($ch);
+        die($error_message);
     }
+
+    curl_close($ch);
 }
+?>
