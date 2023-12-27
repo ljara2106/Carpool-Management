85eab29cef2ce88562c6308d2476ce283056e417
luis jara <xextremex06@gmail.com>
2023-12-27 00:17:22 -0500

    Add function to fetch visitor's IP address and log it along with the current date and time

--- CarPool/validate-captcha.php
+++ CarPool/validate-captcha.php
@@ -2,6 +2,25 @@
 $is_invalid = false;
 $error_message = '';
 
+// Function to get the visitor's IP address
+function getVisitorIP()
+{
+    // If you're testing on localhost, return a placeholder or handle it as needed
+    if ($_SERVER['REMOTE_ADDR'] == '::1' || $_SERVER['REMOTE_ADDR'] == '127.0.0.1') {
+        return 'localhost';
+    }
+
+    // Try to get the visitor's IP address from the X-Forwarded-For header
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        return trim(end($ipAddresses));
+    } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+
+    return '';
+}
+
 if (isset($_POST['cf-turnstile-response'])) {
     $ip = $_SERVER['REMOTE_ADDR'];
     $secret = 'YOUR_SECRET_KEY';
@@ -36,6 +55,23 @@ if (isset($_POST['cf-turnstile-response'])) {
                     session_regenerate_id();
                     $_SESSION["user_id"] = $user["id"];
                     header("Location: index.php");
+                     // Fetching visitor's IP address section ////////////////////////////////
+                     $visitorIP = getVisitorIP();
+                     // Convert UTC time to Eastern Time using DateTime
+                     $utcDateTime = new DateTime($logData, new DateTimeZone('UTC'));
+                     $utcDateTime->setTimezone(new DateTimeZone('America/New_York'));
+                     $easternTime = $utcDateTime->format('Y-m-d H:i:s');
+ 
+                     // Log the IP address along with the current date and time (now in Eastern Time)
+                     $logData = $easternTime . " - IP: $visitorIP\n";
+                     $logFile = 'ip_log.txt';
+                     // Check if the log file exists, and create it if not
+                     if (!file_exists($logFile)) {
+                         touch($logFile);
+                         chmod($logFile, 0640); // Blocks public access to the log file
+                     }
+                     file_put_contents($logFile, $logData, FILE_APPEND);
+                     // Fetching visitor's IP address section ////////////////////////////////
                     exit;
                 }
             }
